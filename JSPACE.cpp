5685601f67d9910e1bda09e678ec5f60cdc2319d
titi_2115 <73474137+jasiukiewicztymon@users.noreply.github.com>
2022-03-23 17:53:59 +0100

    Create JSPACE.cpp

---
+++ JSPACE.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <cctype>
+#include <string>
+
+int main()
+{
+    std::string input;
+
+    while (std::getline(std::cin, input))
+    {
+        for (int i = 0; i < input.size(); i++)
+        {
+            if (input[i] == ' ')
+            {
+                input[i] = 0;
+                input[i + 1] = toupper(input[i + 1]);
+            }
+        }
+        for (int i = 0; i < input.size(); i++)
+        {
+            if (input[i] != 0)
+                std::cout << input[i];
+        }
+        std::cout << "\n";
+    }
+}
